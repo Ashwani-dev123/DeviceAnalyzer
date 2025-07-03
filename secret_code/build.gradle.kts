80c66f39deb354aed82f0e8b3b49b96800147278
Ashwani <ashwani.narayaninfotech@gmail.com>
2025-07-03 17:45:19 +0530

    Add margin code for 15

--- secret_code/build.gradle.kts
+++ secret_code/build.gradle.kts
@@ -93,7 +93,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.Ashwani-dev123"
                 artifactId = "secret_code"
-                version = "1.2.8"
+                version = "1.5.16"
             }
         }
     }
